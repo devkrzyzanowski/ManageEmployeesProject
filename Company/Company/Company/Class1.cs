0f9f98e24626d3b289363e5a0288ccd062b3063b
Michał Krzyżanowski <Miisza95@gmail.com>
2019-04-06 18:58:17 +0200

    init Person class

---
+++ Company/Company/Company/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finances {
+    namespace Company {
+        public class Person {
+        }
+        public class Employee {
+        }
+        public class Operation {
+        }
+    }
+}
---
+++ Company/Company/Company/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company {
+    class Person {
+        private string firstName;
+        private string surName;
+
+        public Person(string firstName, string surName) {
+            this.firstName = firstName;
+            this.surName = surName;
+        }
+
+        public void setFirstName(string firstName) {
+            this.firstName = firstName;
+        }
+        public void setSurName(string surName) {
+            this.surName = surName;
+        }
+        public string getFirstName() {
+            return firstName;
+        }
+        public string getSurName() {
+            return surName;
+        }
+    }
+}
